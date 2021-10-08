e72c58c4ce76766f1cda00ae52679b94d990b367
nazzamp <namwin98@gmail.com>
2021-10-08 09:01:56 +0700

    morning config

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+function Sidebar() {
+  return (
+    <React.Fragment>
+      <StyledContainer>123</StyledContainer>
+    </React.Fragment>
+  );
+}
+
+const StyledContainer = styled.div`
+  background: blue;
+`;
+
+export default Sidebar;
